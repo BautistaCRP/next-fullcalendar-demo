56516995b11e858b187967277f967e6b67d6270f
Bautista Carpintero <bj.carpintero@gmail.com>
2023-04-23 18:11:18 -0300

    testing fullcalendar v6.1.6

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
@@ -6,8 +8,44 @@ import interactionPlugin from '@fullcalendar/interaction';
 import rrulePlugin from '@fullcalendar/rrule';
 
 export default function Home() {
+  const [events, setEvents] = useState<any>([]);
+
+  useEffect(() => {
+    const generate = (now: Date) => {
+      const outEvents = [];
+      for (let i = 0; i < 3; i++) {
+        const start = new Date(now);
+        start.setMinutes(start.getMinutes() + 30 * i);
+        const end = new Date(start);
+        end.setMinutes(start.getMinutes() + 30);
+        const event = {
+          title: `Event ${i}`,
+          start,
+          end,
+        };
+        outEvents.push(event);
+      }
+      return outEvents;
+    };
+
+    const newEvents = [];
+
+    const now = new Date();
+    now.setHours(now.getHours() + 1, 0, 0, 0);
+    newEvents.push(...generate(now));
+
+    now.setDate(now.getDate() + 1);
+    newEvents.push(...generate(now));
+
+    now.setDate(now.getDate() + 1);
+    newEvents.push(...generate(now));
+
+    console.log(events);
+    setEvents(newEvents);
+  }, []);
+
   return (
-    <>
+    <div className='p-2 h-screen'>
       <FullCalendar
         plugins={[
           dayGridPlugin,
@@ -16,12 +54,19 @@ export default function Home() {
           interactionPlugin,
           rrulePlugin,
         ]}
-        initialView='dayGridMonth'
-        events={[
-          { title: 'event 1', date: '2019-04-01' },
-          { title: 'event 2', date: '2019-04-02' },
-        ]}
+        height={'100%'}
+        nowIndicator={true}
+        editable={true}
+        snapDuration={'00:15'}
+        scrollTime={`${new Date().getHours()}:00`}
+        initialView='timeGridWeek'
+        events={events}
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,listWeek',
+        }}
       />
-    </>
+    </div>
   );
 }
